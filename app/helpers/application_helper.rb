1cd93d1262ce4d833cedc7121bd84d01e63be690
Loran Briggs <loranbriggs@gmail.com>
2012-10-30 17:13:33 -0700

    Inital commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,16 @@
+module ApplicationHelper
+  #Returns the title bar on a per-page basis
+    def full_title(page_title)
+        
+        if Rails.env.development?
+            base_title = "Test"
+        else
+            base_title = "Pigeon Post"
+        end
+        if page_title.empty?
+            base_title
+        else
+            "#{base_title} | #{page_title}"
+        end
+    end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PigeonPost::Application.config.secret_token = '16be5579c2e984e38c76ad2ac1b6f56b7c16fe808043354949c48286c5b3160150598d29397345db1e263e540f3c13091c7e5b7be4321efcfa62e2b52aa34386'
